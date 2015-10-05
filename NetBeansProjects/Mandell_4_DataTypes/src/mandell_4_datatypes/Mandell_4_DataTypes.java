4be7bb6e23010a47a5d38f1e0d5a5c1cdd820ed9
RitoMandell <matthew.ryan.mandell@gmail.com>
2015-10-05 12:15:22 -0500

    Merge branch 'master' of https://github.com/RitoMandell/Mandell_4_journal
    # Conflicts:
    #	README.md

---
+++ NetBeansProjects/Mandell_4_DataTypes/src/mandell_4_datatypes/Mandell_4_DataTypes.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mandell_4_datatypes;
+
+/**
+ *
+ * @author mmandell
+ */
+public class Mandell_4_DataTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    //space for coments
+    //used to explain code
+    //code explaination is an expctation
+    //in d22
+    int i = -1;
+    float a = 7.11f;
+    double b = 11.0;
+    short c = 100;
+    char d1= '1';
+    char d2 = 'd';
+    char d3 = 122;
+    String e = "y";
+    boolean f = true; 
+    
+System.out.println(i + " an interger");
+System.out.println(a + " a float");
+System.out.println(b + " a double");
+System.out.println(c + "a short");
+System.out.println(d1 + "a char");         
+    
+    public Mandell_4_DataTypes() {
+        this.f = true;
+        this.f = true;
+        this.b = 0.;
+        this.i = 0;
+    }
+
+
+}
+//+ this adds two numbers and more
+//- this subtracts two numbers and more
+// * this multiplys two numbers and more
+// / divides two numbers and more
+// % this is modulo, shows you the remander
+
